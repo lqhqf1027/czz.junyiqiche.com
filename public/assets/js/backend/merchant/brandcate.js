2e1a0b0abe421ddd5d1413222e74d469d5bb5ae4
yinchuan.com <lqhqf422@gmail.com>
2019-03-01 17:59:40 +0800

    修改

--- public/assets/js/backend/merchant/brandcate.js
+++ public/assets/js/backend/merchant/brandcate.js
@@ -15,12 +15,13 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
             });
 
             var table = $("#table");
-
+            $.fn.bootstrapTable.locales[Table.defaults.locale]['formatSearch'] = function(){return "快速搜索:品牌名称";};
             // 初始化表格
             table.bootstrapTable({
                 url: $.fn.bootstrapTable.defaults.extend.index_url,
                 pk: 'id',
                 sortName: 'id',
+                searchFormVisible: true,
                 columns: [
                     [
                         {checkbox: true},
